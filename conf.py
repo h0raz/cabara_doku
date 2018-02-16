9bd86a9ddae55687592f4ae10fb2aa48841c3dba
Markus Schmid <horaz@loc-com.de>
2018-02-16 09:04:24 +0100

    update doku

--- conf.py
+++ conf.py
@@ -141,7 +141,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Cabara.tex', u'Cabara Documentation',
-     u'Markus Schmid, Michaela Schauer, Betina Schubert', 'manual'),
+     u'Markus Schmid', 'manual'),
 ]
 
 
